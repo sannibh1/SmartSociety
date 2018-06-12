f758fa278f61ad197a419f1548ec8f0d8c23877a
PRANAV GAJERA <pranavgajera1998@gmail.com>
2018-06-12 18:01:19 +0530

    login simplified coding with local host db connection scripts.

--- app/src/main/java/com/example/pranav/smartsociety/Constants.java
+++ app/src/main/java/com/example/pranav/smartsociety/Constants.java
@@ -2,7 +2,7 @@ package com.example.pranav.smartsociety;
 
 public class Constants {
 
-    private static final String ROOT_URL = "http://192.168.0.113/Android/v1/";
+    private static final String ROOT_URL = "http://192.168.43.103/Android/v1/";
 
     public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
     public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
