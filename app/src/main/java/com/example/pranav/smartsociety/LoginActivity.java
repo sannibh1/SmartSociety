8b5bd60d294a5de7979b631e1776f15c9c1d7ce6
PRANAV GAJERA <pranavgajera1998@gmail.com>
2018-06-12 12:03:17 +0530

    login simplified coding with local host db connection scripts.

--- app/src/main/java/com/example/pranav/smartsociety/LoginActivity.java
+++ app/src/main/java/com/example/pranav/smartsociety/LoginActivity.java
@@ -1,62 +1,122 @@
 package com.example.pranav.smartsociety;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
-    private Button login, register;
-    private EditText etEmail, etPass;
-    private DbHelper db;
-    private Session session;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextUsername, editTextPassword;
+    private Button buttonLogin;
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        db = new DbHelper(this);
-        session = new Session(this);
-        login = (Button)findViewById(R.id.btnLogin);
-        register = (Button)findViewById(R.id.btnReg);
-        etEmail = (EditText)findViewById(R.id.etEmail);
-        etPass = (EditText)findViewById(R.id.etPass);
-        login.setOnClickListener(this);
-        register.setOnClickListener(this);
-
-        if(session.loggedin()){
-            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+        if(SharedPrefManager.getInstance(this).isLoggedIn()){
             finish();
+            startActivity(new Intent(this, ProfileActivity.class));
+            return;
         }
+
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please wait...");
+
+        buttonLogin.setOnClickListener(this);
+
+    }
+
+    private void userLogin(){
+        final String username = editTextUsername.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                Constants.URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(),"Done",Toast.LENGTH_LONG).show();
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            if(!obj.getBoolean("error")){
+                                SharedPrefManager.getInstance(getApplicationContext())
+                                        .userLogin(
+                                                obj.getInt("id"),
+                                                obj.getString("username"),
+                                                obj.getString("email")
+                                        );
+                                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                                finish();
+                            }else{
+                                Toast.makeText(
+                                        getApplicationContext(),
+                                       /* obj.getString("message")*/"error",
+                                        Toast.LENGTH_LONG
+                                ).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+
+                        Toast.makeText(
+                                getApplicationContext(),
+                               error.getMessage(),
+                                Toast.LENGTH_LONG
+                        ).show();
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                return params;
+            }
+
+        };
+
+        RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
     }
 
     @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.btnLogin:
-                login();
-                break;
-            case R.id.btnReg:
-                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
-                break;
-            default:
-
-        }
-    }
-
-    private void login(){
-        String email = etEmail.getText().toString();
-        String pass = etPass.getText().toString();
-
-        if(db.getUser(email,pass)){
-            session.setLoggedin(true);
-            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-            finish();
-        }else{
-            Toast.makeText(getApplicationContext(), "Wrong email/password",Toast.LENGTH_SHORT).show();
+    public void onClick(View view) {
+        if(view == buttonLogin){
+            userLogin();
         }
     }
 }
